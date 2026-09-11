ff7bfda50d1ad64f7f9aab4f30e5ed93a4e78bd4
Jack Emond <jackemond@outlook.com>
2026-09-11 00:26:33 -0400

    Cleaned up design to look a little less basic

--- app/src/main/java/com/jemond/workoutapp/HomePage.kt
+++ app/src/main/java/com/jemond/workoutapp/HomePage.kt
@@ -3,13 +3,17 @@ package com.jemond.workoutapp
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.jemond.workoutapp.ui.theme.WorkoutAppTheme
 
 data class Exercise(
@@ -19,8 +23,11 @@ data class Exercise(
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
-    Column(modifier = modifier){
-        Text("Find an Exercise")
+    Column(modifier = Modifier.padding(horizontal = 10.dp, vertical = 20.dp)){
+        Text(
+            "Find an Exercise",
+            fontSize = 25.sp,
+            fontWeight = FontWeight.Bold )
         /* To Do Add a search bar */
         /* To Do add a button to filter by upper or lower body */
         val exercises = listOf(
@@ -47,6 +54,7 @@ fun ListOfExercises(exercises: List<Exercise>) {
             ){
             Text(
                 text = exercise.name,
+                fontSize = 18.sp,
                 modifier = Modifier.weight(1f)
             )
             Button(onClick = { /*TODO*/ }) {
